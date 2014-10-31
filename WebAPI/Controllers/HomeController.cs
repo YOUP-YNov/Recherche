dd4398096953a95861446c60bf5171768d6424ba
yunyun548 <yumiyum_@hotmail.fr>
2014-10-31 14:45:11 +0100

    Nest Install

--- WebAPI/Controllers/HomeController.cs
+++ WebAPI/Controllers/HomeController.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Nest;
 
 namespace MvcApplication1.Controllers
 {
-    public class HomeController : Controller
+    [Authorize(Roles = "Administrator")]
+    public class ManagerController : Controller
     {
-
+        //Adding the ReIndex action. The action return a redirect to the index view.
         public ActionResult Index()
         {
-            return View();
+            return RedirectToAction("Index");
         }
+
+        //Saving connection settings - localhost:9200
+        var setting = new ConnectionSettings("localhost", 9200);
+        //Initializing new Client
+        var client = new ElasticClient(setting);
     }
 }
