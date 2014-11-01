fe1ca4f9c46bc38896b0e45a4c6df6ddef21b195
MartinLG <martin.leguillou@y-nov.com>
2014-11-02 00:01:38 +0100

    script migration des Blog fait selon le schema de Jordi, à voir ensemble

--- Tools/MigrationScript.cs
+++ Tools/MigrationScript.cs
@@ -8,6 +8,8 @@ using MvcApplication1;
 using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
+using MvcApplication1;
+using MvcApplication1.Controllers;
 
 namespace Tools
 {
@@ -18,13 +20,35 @@ namespace Tools
             ElasticClient elastic = YoupElasticSearch.InitializeConnection();
             using (var context = new YoupDEVEntities())
             {
-                var test = context.BLOG_Blog.SqlQuery("Select * from BLOG_Blog").ToList<BLOG_Blog>();
-
-                foreach (var blog in test)
+                var blogs = (from b in context.BLOG_Blog
+                               select b).ToList<BLOG_Blog>();
+                foreach (var blog in blogs)
                 {
-                    Console.WriteLine(blog.TitreBlog);
+                    Blog blogElastic = new Blog(blog.Blog_id.ToString(), blog.TitreBlog, blog.Theme_id.ToString());
+                    var indexB = elastic.Index(blogElastic);
+                    /* var visits = (from v in context.BLOG_Visite
+                                  where v.Blog_Id == blog.Blog_id
+                                  select v).Count();*/
+
+                    var articles = (from a in context.BLOG_Article
+                                    where a.Blog_id == blog.Blog_id
+                                    select a).ToList<BLOG_Article>();
+
+                    foreach (var article in articles)
+                    {
+                        BlogPost articleElastic = new BlogPost(article.Article_id.ToString(), article.ContenuArticle, blog.Utilisateur_id.ToString(), article.TitreArticle);
+                        var indexA = elastic.Index(articleElastic);
+
+                        var comments = (from c in context.BLOG_Commentaire
+                                        where c.Article_id == article.Article_id
+                                        select c).ToList<BLOG_Commentaire>();
+                        foreach (var comment in comments)
+                        {
+                            BlogPostComment commentElastic = new BlogPostComment(comment.Commentaire_id.ToString(), comment.ContenuCommentaire, comment.Utilisateur_id.ToString());
+                            var indexC = elastic.Index(commentElastic);
+                        }
+                    }
                 }
-                Console.ReadLine();
             }
         }
     }
