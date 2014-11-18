8cde5520b4bd35c1e5c812d997ba68717dd13330
DNS22 <tesnerantoine@gmail.com>
2014-11-18 18:29:00 +0100

    changement param get dans addController

--- WebAPI/Controllers/addController.cs
+++ WebAPI/Controllers/addController.cs
@@ -15,69 +15,60 @@ namespace MvcApplication1.Controllers
     public class addController : ApiController
     {
 
-        public bool get_blog()
+        public bool get_blog(string id = "", string content = "", string category = "")
         {
-            var nvc = HttpUtility.ParseQueryString(Request.RequestUri.Query);
-            Blog XBlog = new Blog(nvc["id"], nvc["content"], nvc["category"]);
+            Blog XBlog = new Blog(id, content, category);
             blogController controller = new blogController();
             controller.AddBlog(XBlog);
             return true;
         }
 
-        public bool get_blogpost()
+        public bool get_blogpost(string id = "", string content = "", string author = "", string title ="")
         {
-            var nvc = HttpUtility.ParseQueryString(Request.RequestUri.Query);
-            BlogPost XBlogPost = new BlogPost(nvc["id"], nvc["content"], nvc["author"], nvc["title"]);
+            BlogPost XBlogPost = new BlogPost(id, content, author, title);
             blogController controller = new blogController();
             controller.AddBlogPost(XBlogPost);
             return true;
         }
 
-        public bool get_blogpostcomment()
+        public bool get_blogpostcomment(string id="", string content="", string author="")
         {
-            var nvc = HttpUtility.ParseQueryString(Request.RequestUri.Query);
-            BlogPostComment XBlogPostComment = new BlogPostComment(nvc["id"], nvc["content"], nvc["author"]);
+            BlogPostComment XBlogPostComment = new BlogPostComment(id, content, author);
             blogController controller = new blogController();
             controller.AddBlogPostComment(XBlogPostComment);
             return true;
         }
 
-        public bool get_event()
+        public bool get_event(string idP="", string nameP="", string town="", decimal latitude=0, decimal longitude=0,
+            string idE="", string nameE="", long type=0L, DateTime date = new DateTime(), string adresse="")
         {
-            var nvc = HttpUtility.ParseQueryString(Request.RequestUri.Query);
-            decimal? latitude = decimal.Parse(nvc["latitude"]);
-            decimal? longitude = decimal.Parse(nvc["longitude"]);
-            Place XPlace = new Place(nvc["id"], nvc["name"], nvc["town"], latitude, longitude);
-            Event XEvent = new Event(nvc["id"], nvc["name"], long.Parse(nvc["type"]), DateTime.Parse(nvc["date"]), XPlace, nvc["adresse"]);
+            Place XPlace = new Place(idP, nameP, town, latitude, longitude);
+            Event XEvent = new Event(idE, nameE, type, date, XPlace, adresse);
             eventsController controller = new eventsController();
             controller.AddEvent(XEvent);
             return true;
         }
 
-        public bool get_place()
+        public bool get_place(string id = "", string name = "", string town = "", decimal latitude = 0, decimal longitude = 0)
         {
-            var nvc = HttpUtility.ParseQueryString(Request.RequestUri.Query);
-            decimal? latitude = decimal.Parse(nvc["latitude"]);
-            decimal? longitude = decimal.Parse(nvc["longitude"]);
-            Place XPlace = new Place(nvc["id"], nvc["name"], nvc["town"], latitude, longitude);
+            Place XPlace = new Place(id, name, town, latitude, longitude);
             placesController controller = new placesController();
             controller.AddPlace(XPlace);
             return true;
         }
 
-        public bool get_profile()
+        public bool get_profile(string id="", string firstname="", string lastname="", string pseudo="", string activity="",
+            int age=0, bool sex=true, string town="")
         {
-            var nvc = HttpUtility.ParseQueryString(Request.RequestUri.Query);
-            Profile XProfile = new Profile(nvc["id"], nvc["firstname"], nvc["lastName"], nvc["pseudo"], nvc["activity"], Int32.Parse(nvc["age"]), bool.Parse(nvc["sex"]), nvc["town"]);
+            Profile XProfile = new Profile(id, firstname, lastname, pseudo, activity, age, sex, town);
             profilesController controller = new profilesController();
             controller.AddProfile(XProfile);
             return true;
         }
 
-        public bool get_postforum()
+        public bool get_postforum(string id="", string board="", string content="", DateTime date = new DateTime(), string author="")
         {
-            var nvc = HttpUtility.ParseQueryString(Request.RequestUri.Query);
-            PostForum XPostForum = new PostForum(nvc["id"], nvc["board"], nvc["content"], DateTime.Parse(nvc["date"]), nvc["author"]);
+            PostForum XPostForum = new PostForum(id, board, content, date, author);
             forumController controller = new forumController();
             controller.AddPostForum(XPostForum);
             return true;
